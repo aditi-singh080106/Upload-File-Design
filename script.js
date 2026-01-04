3a8dc42326981732fd18be634ffaba2970c751cb
Aditi Singh <aditi08092003@gmail.com>
2026-01-05 00:42:07 +0530

    Updated Script.js File

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ btn.addEventListener("click",function(){
     inp.click();
 });
 inp.addEventListener("change",function(dets){
-    btn.textContent=`${dets.target?.files[0].name}`;
+    // btn.textContent=`${dets.target?.files[0].name}`;
+    const file = dets.target.files[0];
+    if(file){
+        btn.textContent=file.name;
+    }
 })
